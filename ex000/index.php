c1e926f10ca397e5ee6679eb6c6bfc7d09c6a478
Renan Garcia <renanrgarcia@gmail.com>
2024-03-01 07:14:56 -0300

    Hello world PHP

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Getting rid from the curse</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello world! \u{1F30E}"; // Caractere unicode
+            // print "Hello word";
+        ?>
+    </h1>
+    <p>Let's try to get rid of the curse</p>
+</body>
+</html>
